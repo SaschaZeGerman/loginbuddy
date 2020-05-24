ecd7238898f9448ae604a640e4e29108bd013987
Sascha Preibisch <sascha.preibisch@gmail.com>
2020-05-23 20:39:41 -0700

    feature/sidecat-optional-ssl

--- docker-build/add-ons/sidecar/loginbuddy.sh
+++ docker-build/add-ons/sidecar/loginbuddy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# configures the environment variables HOSTNAME_LOGINBUDDY to 'loginbuddy-sidecar' and SSL_PORT to 444
-#
 printf "===============\n"
-printf "== Loginbuddy: Overwriting HOSTNAME_LOGINBUDDY and SSL_PORT to loginbuddy-sidecar and 444\n"
+printf "== Loginbuddy: Overwriting HOSTNAME_LOGINBUDDY and PORTs to loginbuddy-sidecar and 444 (SSL) and 8044 (http)\n"
 printf "== Loginbuddy: Currently these values are required for this setup\n"
 printf "===============\n"
 
 export HOSTNAME_LOGINBUDDY=loginbuddy-sidecar
 export SSL_PORT=444
+export HTTP_PORT=8044
 
 # generating a UUID as password for the generated keystore
 #
@@ -17,14 +16,6 @@ UUID=$(cat /proc/sys/kernel/random/uuid)
 # Create private key
 #
 keytool -genkey -alias loginbuddy -keystore /usr/local/tomcat/ssl/loginbuddy.p12 -storetype PKCS12 -keyalg RSA -storepass ${UUID} -keypass ${UUID} -validity 90 -keysize 2048 -dname "CN=${HOSTNAME_LOGINBUDDY}"
-
-# Export the public certificates
-#
-#keytool -export -alias loginbuddy -file /usr/local/tomcat/ssl/loginbuddy.crt -keystore /usr/local/tomcat/ssl/loginbuddy.p12 -storepass ${UUID}
-
-# Import the certs as trusted certificates
-#
-#keytool -importcert -alias loginbuddy -file /usr/local/tomcat/ssl/loginbuddy.crt -storepass changeit -keystore $JAVA_HOME/lib/security/cacerts -trustcacerts -noprompt
 
 # Find the policy file that contains socket permissions and add them to the default catalina.policy file
 # default is located here: /usr/local/tomcat/conf/catalina.policy
@@ -39,6 +30,7 @@ export CATALINA_OPTS="${SYSTEM_PROPS} -Dorg.jose4j.jws.default-allow-none=false"
 #
 sed -i "s/@@hostname@@"/${HOSTNAME_LOGINBUDDY}/g /usr/local/tomcat/conf/server.xml
 sed -i "s/@@sslport@@"/${SSL_PORT}/g /usr/local/tomcat/conf/server.xml
+sed -i "s/@@httpport@@"/${HTTP_PORT}/g /usr/local/tomcat/conf/server.xml
 sed -i "s/@@sslpwd@@"/${UUID}/g /usr/local/tomcat/conf/server.xml
 
 # check if dynamic registration should be supported
@@ -49,6 +41,7 @@ sh /opt/docker/loginbuddy_oidcdr_import.sh
 #
 export HOSTNAME_LOGINBUDDY=
 export SSL_PORT=
+export HTTP_PORT=
 export UUID=
 
 # run the original tomcat entry point command as specified in tomcat's Dockerfile
--- net.loginbuddy.sidecar/src/main/java/net/loginbuddy/sidecar/server/SidecarMaster.java
+++ net.loginbuddy.sidecar/src/main/java/net/loginbuddy/sidecar/server/SidecarMaster.java
@@ -8,8 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 public class SidecarMaster extends HttpServlet {
 
   static void checkClientConnection(HttpServletRequest request) throws IllegalAccessException {
-    // port 444 should only be available via loginbuddy-sidecar
-    if (! ("loginbuddy-sidecar".equals(request.getServerName()) && request.getLocalPort() == 444) ) {
+    // port 444 (or 8044 for http) should only be available via loginbuddy-sidecar
+    if (! ("loginbuddy-sidecar".equals(request.getServerName()) && (request.getLocalPort() == 444 || request.getLocalPort() == 8044 ))) {
       throw new IllegalAccessException(HttpHelper.getErrorAsJson("invalid_client", "connection not accepted").toJSONString());
     }
   }
