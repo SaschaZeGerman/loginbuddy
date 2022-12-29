1cee22bd410a160c3c9984caf03b99b4ebe25927
SaschaZeGerman <sascha.preibisch@gmail.com>
2022-12-29 12:51:49 -0800

    maintenance, refactored code

--- net.loginbuddy.service/src/main/java/net/loginbuddy/service/server/Overlord.java
+++ net.loginbuddy.service/src/main/java/net/loginbuddy/service/server/Overlord.java
@@ -5,11 +5,7 @@ import jakarta.servlet.http.HttpServlet;
 import net.loginbuddy.common.api.HttpHelper;
 import net.loginbuddy.config.loginbuddy.OverlordMaster;
 
-import java.util.logging.Logger;
-
 public class Overlord extends HttpServlet {
-
-    private static Logger LOGGER = Logger.getLogger(String.valueOf(Overlord.class));
 
     @Override
     public void init() throws ServletException {
